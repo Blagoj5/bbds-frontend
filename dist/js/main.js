70d13d79b66f3d72cbc95143842c0ae828f4b1cf
Blagoj5 <blagoj.petrov5@gmail.com>
2020-02-09 03:37:31 +0100

    completed without responsive

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+// $(document).foundation("reflow");
+
+// $(function() {
+//     $(".normal").bind("click", function(event) {
+//         $(".normal").toggleClass("liked");
+//     });
+// });
+
+function myFunction(x) {
+    x.classList.toggle("liked");
+}
